e7b3861b857529e3be29800f220687c048e3e3df
NDK-ITer <116534879+NDK-ITer@users.noreply.github.com>
2023-04-11 14:20:30 +0700

    Add project files.

---
+++ DrugStores/Areas/Identity/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using DrugStores.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using DrugStores.Models.ObjectDB;
+
+namespace DrugStores.Areas.Identity.Data;
+
+public class ApplicationDbContext : IdentityDbContext<DrugStoresUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+
+    public DbSet<DrugStores.Models.ObjectDB.HoaDon> HoaDon { get; set; } = default!;
+}
