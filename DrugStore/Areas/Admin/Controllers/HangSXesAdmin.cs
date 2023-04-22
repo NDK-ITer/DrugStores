3ddfb3f1ff9ed764b996fb493ee84edf5ca618e6
NDK-ITer <116534879+NDK-ITer@users.noreply.github.com>
2023-04-22 20:55:24 +0700

    HangSX xong

---
+++ DrugStore/Areas/Admin/Controllers/HangSXesAdmin.cs
@@ -0,0 +1,98 @@
+﻿using DrugStore.Models.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DrugStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HangSXesAdmin : Controller
+    {
+        private readonly DrugStoreDbContext dbContext = new DrugStoreDbContext();
+        // GET: HangSXesAdmin
+        public ActionResult Index()
+        {
+            List<HangSX> hangSXes = dbContext.HangSXes.ToList();
+            return View(hangSXes);
+        }
+
+        // GET: HangSXesAdmin/Details/5
+        public ActionResult Details(int id)
+        {
+            HangSX hangSX = dbContext.HangSXes.Find(id);
+            return View(hangSX);
+        }
+
+        // GET: HangSXesAdmin/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: HangSXesAdmin/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(HangSX hangSX)
+        {
+            try
+            {
+                dbContext.HangSXes.Add(hangSX);
+                dbContext.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HangSXesAdmin/Edit/5
+        public ActionResult Edit(int id)
+        {
+            HangSX hangSX = dbContext.HangSXes.Find(id);
+            return View(hangSX);
+        }
+
+        // POST: HangSXesAdmin/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(HangSX hangSX)
+        {
+            try
+            {
+                dbContext.Entry(hangSX).State = EntityState.Modified;
+                dbContext.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HangSXesAdmin/Delete/5
+        public ActionResult Delete(int id)
+        {
+            HangSX hangSX = dbContext.HangSXes.Find(id);
+            return View(hangSX);
+        }
+
+        // POST: HangSXesAdmin/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            try
+            {
+                HangSX hangSX = dbContext.HangSXes.Find(id);
+                dbContext.HangSXes.Remove(hangSX);
+                dbContext.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
