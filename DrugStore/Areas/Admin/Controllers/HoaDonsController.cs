3649df11e0ed720f7ddae265e77159b8f26d7c56
ely.nguyen <ely.nguyen@tpfgroup.cc>
2023-04-30 00:16:53 +0700

    admin

---
+++ DrugStore/Areas/Admin/Controllers/HoaDonsController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DrugStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HoaDonsController : Controller
+    {
+        // GET: HoaDonsController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: HoaDonsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: HoaDonsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: HoaDonsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HoaDonsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: HoaDonsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HoaDonsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: HoaDonsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- DrugStore/Areas/Admin/Controllers/SanPhamsAdminController.cs
+++ DrugStore/Areas/Admin/Controllers/SanPhamsAdminController.cs
@@ -1,5 +1,7 @@
-﻿using DrugStore.Models.Entities;
+﻿using DrugStore.Models;
+using DrugStore.Models.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 
 namespace DrugStore.Areas.Admin.Controllers
@@ -8,11 +10,50 @@ namespace DrugStore.Areas.Admin.Controllers
     public class SanPhamsAdminController : Controller
     {
         private readonly DrugStoreDbContext dbContext = new DrugStoreDbContext();
-        public ActionResult Index()
+        public async Task<IActionResult> Index(string sortOrder,
+    string currentFilter,
+    string searchString,
+    int? pageNumber)
         {
-            List<SanPham> sanPhams = dbContext.SanPhams.Include(s => s.HangSX).Include(s => s.LoaiSP).Include(s => s.TrangThai).ToList();
+            ViewData["CurrentSort"] = sortOrder;
+            ViewData["NameSortParm"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewData["DateSortParm"] = sortOrder == "Date" ? "date_desc" : "Date";
 
-            return View(sanPhams);
+            if (searchString != null)
+            {
+                pageNumber = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+            ViewData["CurrentFilter"] = searchString;
+
+            var sanPhams = from s in dbContext.SanPhams select s;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                sanPhams = sanPhams.Where(s => s.TenSP.Contains(searchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    sanPhams = sanPhams.OrderByDescending(s => s.TenSP);
+                    break;
+                case "Date":
+                    sanPhams = sanPhams.OrderBy(s => s.NgayTao);
+                    break;
+                case "date_desc":
+                    sanPhams = sanPhams.OrderByDescending(s => s.NgayTao);
+                    break;
+                default:
+                    sanPhams = sanPhams.OrderBy(s => s.TenSP);
+                    break;
+            }
+
+            int pageSize = 3;
+            return View(await PaginatedList<SanPham>.CreateAsync(sanPhams.AsNoTracking(), pageNumber ?? 1, pageSize));
         }
     }
 }
