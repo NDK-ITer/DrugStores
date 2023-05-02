431ee8c6898b1cc6f8f3d6e501646318b5db3803
NDK-ITer <116534879+NDK-ITer@users.noreply.github.com>
2023-05-02 09:29:55 +0700

    nope

---
+++ DrugStore/Models/ViewModel/TrangChuViewModel.cs
@@ -0,0 +1,20 @@
+﻿using DrugStore.Models.Entities;
+
+namespace DrugStore.Models.ViewModel
+{
+    public class TrangChuViewModel
+    {
+        private readonly DrugStoreDbContext dbContext = new DrugStoreDbContext();
+        public List<SanPham> dsSanPhamNoiBat { get; set; }
+        public List<TinTuc> tinTucs { get; set; }
+        public List<LoaiSP> loaiSPs { get; set; }
+        public List<LoaiThuoc> loaiThuocs { get; set; }
+
+        public TrangChuViewModel()
+        {
+            loaiThuocs = dbContext.LoaiThuocs.ToList();
+            loaiSPs = dbContext.LoaiSPs.ToList();
+            dsSanPhamNoiBat = dbContext.SanPhams.ToList();
+        }
+    }
+}
