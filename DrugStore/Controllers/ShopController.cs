97b9c6e83f953dcdaa6ad2e23eeda7af14f27c41
NDK-ITer <116534879+NDK-ITer@users.noreply.github.com>
2023-05-04 07:50:00 +0700

    nope

--- DrugStore/Controllers/ShopController.cs
+++ DrugStore/Controllers/ShopController.cs
@@ -4,16 +4,23 @@ using Microsoft.EntityFrameworkCore;
 using System.Net.NetworkInformation;
 using X.PagedList;
 using X.PagedList.Mvc;
-
-
+using Microsoft.AspNetCore.Identity;
+using DrugStore.Areas.Identity.Data;
+using Microsoft.AspNetCore.Authorization;
 
 namespace DrugStore.Controllers
 {
     public class ShopController : Controller
     {
         private readonly DrugStoreDbContext dbContext =  new DrugStoreDbContext().Created();
-        
-        
+        List<GioHang> gioHangs;
+
+        private UserManager<AppNetUser> _userManager;
+        public ShopController(UserManager<AppNetUser> userManager)
+        {
+            _userManager = userManager;
+            //var user = userManager.Users.ToList();
+        }
         public IActionResult Index(int page = 1)
         {
             page  = page < 1 ? 1 : page;
@@ -22,17 +29,24 @@ namespace DrugStore.Controllers
         }
         public IActionResult Product(Guid id)
         {
-
             SanPham sanPham = dbContext.SanPhams.Find(id);
-            if (sanPham.MaLoaiSP == "T")
-            {
-                Thuoc thuoc = dbContext.Thuocs.Find(id) ;
-            }
             return View(sanPham);
         }
 
+        public void TakeShopingcart(string idUser)
+        {
+            
+            gioHangs = dbContext.GioHangs.Where(s => s.Id == idUser).ToList();
+            if (gioHangs == null)
+            {
+                gioHangs = new List<GioHang>();
+            }
+        }
+
+        [Authorize]
         public IActionResult Shopingcart()
         {
+            TakeShopingcart(_userManager.GetUserId(User));
             return View();
         }
         public IActionResult CheckOut()
