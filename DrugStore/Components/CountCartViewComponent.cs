594fe9f68a7ee99ba37d2d6b7f3724df3e6ebf70
NDK-ITer <116534879+NDK-ITer@users.noreply.github.com>
2023-05-05 15:00:29 +0700

    nope

---
+++ DrugStore/Components/CountCartViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using DrugStore.Areas.Identity.Data;
+using DrugStore.Models.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;using Microsoft.AspNetCore.Authorization;
+
+
+namespace DrugStore.Components
+{
+    public class CountCartViewComponent: ViewComponent
+    {
+        private readonly DrugStoreDbContext dbContext = new DrugStoreDbContext().Created();
+        
+        public async Task<IViewComponentResult> InvokeAsync(string id)
+        {
+            List<GioHang> gioHangs = dbContext.GioHangs.Where(s => s.Id == id).ToList();
+            ViewBag.CountCart = gioHangs.Count();
+            return View();
+        }
+    }
+}
---
+++ DrugStore/Components/SumPriceCartViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using DrugStore.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DrugStore.Components
+{
+    public class SumPriceCartViewComponent:ViewComponent
+    {
+        private readonly DrugStoreDbContext dbContext = new DrugStoreDbContext().Created();
+
+        public async Task<IViewComponentResult> InvokeAsync(string id)
+        {
+            List<GioHang> gioHangs = dbContext.GioHangs.Where(s => s.Id == id).ToList();
+            ViewBag.SumPriceCart = gioHangs.Sum(c => c.ThanhTien);
+            return View();
+        }
+    }
+}
