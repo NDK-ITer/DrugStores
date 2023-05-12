93639101b5b1776140b5e3d8ef24dafffeec3fea
NDK-ITer <116534879+NDK-ITer@users.noreply.github.com>
2023-05-12 15:22:02 +0700

    nope

---
+++ DrugStore/Components/SanPhamLienQuanViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using DrugStore.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+using NuGet.Packaging;
+
+namespace DrugStore.Components
+{
+    public class SanPhamLienQuanViewComponent : ViewComponent
+    {
+        private readonly DrugStoreDbContext dbContext = new DrugStoreDbContext().Created();
+        public async Task<IViewComponentResult> InvokeAsync(Guid maLSP, int? maHSX)
+        {
+            List<SanPham> sanPhamLienQuans = dbContext.SanPhams.Where(s => s.Thuoc.MaLT == maLSP).ToList();
+            sanPhamLienQuans.AddRange(dbContext.SanPhams.Where(c => c.MaHSX ==  maHSX).ToList());
+            return View(sanPhamLienQuans);
+        }
+    }
+}
