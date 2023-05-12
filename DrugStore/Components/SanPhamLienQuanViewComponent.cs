437ce063387310539d8d07833dcd03ae61c4452c
NDK-ITer <116534879+NDK-ITer@users.noreply.github.com>
2023-05-12 15:44:05 +0700

    nope

--- DrugStore/Components/SanPhamLienQuanViewComponent.cs
+++ DrugStore/Components/SanPhamLienQuanViewComponent.cs
@@ -7,10 +7,18 @@ namespace DrugStore.Components
     public class SanPhamLienQuanViewComponent : ViewComponent
     {
         private readonly DrugStoreDbContext dbContext = new DrugStoreDbContext().Created();
-        public async Task<IViewComponentResult> InvokeAsync(Guid maLSP, int? maHSX)
+        public async Task<IViewComponentResult> InvokeAsync(Guid idSP, Guid maLSP, int? maHSX)
         {
-            List<SanPham> sanPhamLienQuans = dbContext.SanPhams.Where(s => s.Thuoc.MaLT == maLSP).ToList();
-            sanPhamLienQuans.AddRange(dbContext.SanPhams.Where(c => c.MaHSX ==  maHSX).ToList());
+            List<SanPham> sanPhamLienQuans = dbContext.SanPhams.Where(s => s.Thuoc.MaLT == maLSP && s.MaSP != idSP).ToList();
+            foreach (var item in sanPhamLienQuans)
+            {
+                SanPham newItem = dbContext.SanPhams.FirstOrDefault(c => c.MaHSX == maHSX);
+                if (sanPhamLienQuans.Contains(newItem) == false)
+                {
+                    sanPhamLienQuans.Add(newItem);
+                }
+            }
+            ViewBag.Count = sanPhamLienQuans.Count;
             return View(sanPhamLienQuans);
         }
     }
