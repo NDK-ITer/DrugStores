523f6b60e922550182f88b60e81ad0251e7aa652
NDK-ITer <116534879+NDK-ITer@users.noreply.github.com>
2023-05-12 10:58:40 +0700

    đề xuất sản phẩm

--- DrugStore/Components/CaNhanHoaViewComponent.cs
+++ DrugStore/Components/CaNhanHoaViewComponent.cs
@@ -9,19 +9,30 @@ namespace DrugStore.Components
         private readonly DrugStoreDbContext dbContext = new DrugStoreDbContext().Created();
         public async Task<IViewComponentResult> InvokeAsync(string idUser, int? page)
         {
+            List<SanPham> dsSanPhamDeXua = new List<SanPham>();
             if (idUser != null)
             {
                 if (page == null) { page = 1; }
                 page = page < 1 ? 1 : page;
                 int pageSize = 9;
-                List<CT_CaNhanHoa> cT_CaNhanHoa = dbContext.CT_CaNhanHoas.Where(c => c.Id == idUser).OrderBy(c => c.SoLanXem).Take(5).ToList();
-                TongHopLoaiSP tongHopLoaiSP = new TongHopLoaiSP();
-                foreach (var item in cT_CaNhanHoa)
+                List<CT_CaNhanHoa> cT_CaNhanHoas = dbContext.CT_CaNhanHoas.Where(c => c.Id == idUser).OrderBy(c => c.SoLanXem).Take(5).ToList();
+                foreach (var item in cT_CaNhanHoas)
                 {
-
+                    TongHopLoaiSP newItem = dbContext.TongHopLoaiSPs.Find(item.MaTHLSP);
+                    if (newItem != null)
+                    {
+                        if (newItem.LoaiThuoc != null)
+                        {
+                            LoaiThuoc loaiThuoc = newItem.LoaiThuoc;
+                            foreach (var itemThuoc in loaiThuoc.Thuocs)
+                            {
+                                dsSanPhamDeXua.Add(itemThuoc.SanPham);
+                            }
+                        }
+                    }
                 }
             }
-            return View();
+            return View(dsSanPhamDeXua);
         }
     }
 }
