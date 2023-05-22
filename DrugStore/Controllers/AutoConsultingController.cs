6a77ce72e2b4d830db45955042bc71271a9c04cb
NDK-ITer <116534879+NDK-ITer@users.noreply.github.com>
2023-05-22 10:54:35 +0700

    Xong phần tư vấn

--- DrugStore/Controllers/AutoConsultingController.cs
+++ DrugStore/Controllers/AutoConsultingController.cs
@@ -1,12 +1,61 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DrugStore.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using X.PagedList;
 
 namespace DrugStore.Controllers
 {
     public class AutoConsultingController : Controller
     {
-        public IActionResult Consulting(string consultString)
+        private readonly DrugStoreDbContext dbContext = new DrugStoreDbContext().Created();
+
+        public IActionResult Index(int? page, List<SanPham>? sanPhams)
+        {
+            if (sanPhams == null || sanPhams.Count <= 0)
+            {
+                sanPhams = dbContext.SanPhams.Where(s => s.MaTT == 1).OrderBy(s => s.TenSP).ToList();
+            }
+            if (page == null) { page = 1; }
+            page = page < 1 ? 1 : page;
+            int pageSize = 6;
+            return View(sanPhams.ToPagedList((int)page, pageSize));
+        }
+        public IActionResult Consulting()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Consulting(string consultString)
+        {
+            if (consultString.IsNullOrEmpty())
+            {
+                return View();
+            }
+            string[] temp = consultString.Trim().ToUpper().Split(' ');
+            List<string> worldList = temp.ToList();
+            var dsSP = dbContext.SanPhams.ToList();
+            foreach (string world in worldList.ToList())
+            {
+                if (string.IsNullOrEmpty(world))
+                {
+                    break;
+                }
+                foreach (var item in dsSP.ToList())
+                {
+                    if (!(item.CongDung.ToUpper().Contains(world) || item.TenSP.ToUpper().Contains(world) || item.MoTa.ToUpper().Contains(world)))
+                    {
+                        dsSP.Remove(item);
+                    }
+                }
+                if (dsSP.IsNullOrEmpty())
+                {
+                    dsSP = dbContext.SanPhams.ToList();
+                }
+                worldList.Remove(world);
+            }
+            
+            return RedirectToAction("Index", dsSP);
         }
     }
 }
