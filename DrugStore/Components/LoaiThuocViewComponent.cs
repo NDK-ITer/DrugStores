ae3331c5bf4b507b8a0407c8a0bf15b83aad376c
NDK-ITer <116534879+NDK-ITer@users.noreply.github.com>
2023-05-27 11:16:27 +0700

    nope

---
+++ DrugStore/Components/LoaiThuocViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using DrugStore.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DrugStore.Components
+{
+    public class LoaiThuocViewComponent: ViewComponent
+    {
+        private readonly DrugStoreDbContext dbContext = new DrugStoreDbContext().Created();
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(dbContext.LoaiThuocs.OrderByDescending(c => c.TenLoaiThuoc).ToList());
+        }
+    }
+}
---
+++ DrugStore/Components/SanPhamMoiViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using DrugStore.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DrugStore.Components
+{
+    public class SanPhamMoiViewComponent : ViewComponent
+    {
+        private readonly DrugStoreDbContext dbContext = new DrugStoreDbContext().Created();
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(dbContext.SanPhams.OrderByDescending(c => c.NgayTao).Take(4).ToList());
+        }
+    }
+}
