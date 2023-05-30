96f3480f5836fcf5e23ed357e156e16396fc48e4
NDK-ITer <116534879+NDK-ITer@users.noreply.github.com>
2023-05-30 21:01:19 +0700

    nope

---
+++ DrugStore/Components/TagViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using DrugStore.Models.Entities;
+using DrugStore.Models.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DrugStore.Components
+{
+    public class TagViewComponent:ViewComponent
+    {
+        private readonly DrugStoreDbContext dbContext = new DrugStoreDbContext().Created();
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(dbContext.Tags.ToList());
+        }
+    }
+}
