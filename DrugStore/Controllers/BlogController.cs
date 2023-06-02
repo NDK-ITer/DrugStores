4f434a41eecacf32a3e0beddb353ec5064e0adab
NDK-ITer <116534879+NDK-ITer@users.noreply.github.com>
2023-06-02 12:15:35 +0700

    Cập nhập tin tức 2

--- DrugStore/Controllers/BlogController.cs
+++ DrugStore/Controllers/BlogController.cs
@@ -7,20 +7,36 @@ namespace DrugStore.Controllers
     public class BlogController : Controller
     {
         private readonly DrugStoreDbContext dbContext = new DrugStoreDbContext().Created();
-        public IActionResult Index(string? search, int? page)
+        public IActionResult Index(string? search, int? page, string? idTag)
         {
-            List<TinTuc> dsTinTuc = null;
+            int pageSize = 6;
+            List<TinTuc> dsTinTuc;
             if (search != null)
             {
                 dsTinTuc = dbContext.TinTucs.Where(c => c.MoTaTT.Contains(search)).ToList();
+                if (dsTinTuc == null)
+                {
+                    dsTinTuc = new List<TinTuc>();
+                }
+                if (page == null) { page = 1; }
+                page = page < 1 ? 1 : page;
+                return View(dsTinTuc.ToPagedList((int)page, pageSize));
             }
+            if (idTag != null)
+            {
+                Tag tag = dbContext.Tags.Find(idTag);
+                dsTinTuc = tag.TinTucs;
+                if (dsTinTuc == null)
+                {
+                    dsTinTuc = new List<TinTuc>();
+                }
+                if (page == null) { page = 1; }
+                page = page < 1 ? 1 : page;
+                return View(dsTinTuc.ToPagedList((int)page, pageSize));
+            }
+            dsTinTuc = dbContext.TinTucs.ToList();
             if (page == null) { page = 1; }
             page = page < 1 ? 1 : page;
-            int pageSize = 6;
-            if (search == null)
-            {
-                dsTinTuc = dbContext.TinTucs.ToList();
-            }
             return View(dsTinTuc.ToPagedList((int)page, pageSize));
         }
 
