f85188cc1e5fec6de62f8ad70df6b8f113857ab7
DucHuy245 <127472090+DucHuy245@users.noreply.github.com>
2023-06-05 20:32:04 +0700

    commit

--- DrugStore/Areas/Admin/Data/TinTucInput.cs
+++ DrugStore/Areas/Admin/Data/TinTucInput.cs
@@ -1,13 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace DrugStore.Areas.Admin.Data
 {
     public class TinTucInput
     {
         public Guid idtintuc { get; set; }
+        [DisplayName("Nội dung")]
+        [Required]
         public string content { get; set; }
+        [DisplayName("Hình ảnh")]
+        [Required]
         public string cover { get; set; }
+        [DisplayName("Mô tả")]
+        [Required]
         public string description { get; set; }
+        [DisplayName("Ngày tạo")]
         public DateTime? createdDate { get; set; }
         public string[] idtag  { get; set;}
         public List<SelectListItem> drptag { get; set; }    
